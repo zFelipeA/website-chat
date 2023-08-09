eaf7b3ff3654a02535bac9ab29bed89337159914
Felps157 <lipeaugustocastro@gmail.com>
2023-08-09 19:16:07 -0300

    TODO: Attempt to establish WebSocket connection with Railway

--- src/models/socket.js
+++ src/models/socket.js
@@ -49,7 +49,7 @@ export default async function verifySetup(res) {
         return;
     }
 
-    const server = new WebSocketServer({ port: 443 }); //80
+    const server = new WebSocketServer({ port: 80 }); //80
 
     server.on("connection", socket => {
         newClient(socket);
